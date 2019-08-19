c7d9a499605ba0afb6a67e3de684eb1921cdd707
Reberth <reberth.santos@unifesp.br>
2019-08-18 21:41:21 -0300

    adicionei o hiper link do exercico 1

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 
     <body>
         <h1>Lista de exercicios:</h1>
+        <p>Link: https://b5cbb579-a-62cb3a1a-s-sites.googlegroups.com/site/desenvolvimentowebi20121/material-de-apoio/listas-de-exercicios/listas-de-exercicios/lista_exercicios_01.pdf?attachauth=ANoY7cr9WLuE8B230G0ZMJjeYZhI4RHp8fw19pWevM1wAgqxSfb_2c2nRhSSLEROcoxbQqaTgtGhtcPCPdk0jyndncsuKov3VTmWa8oCftskDJKR-qnJW_6fevQJYAQT2v8-QafHgyvm-VOffr9ZmZbxX2iRvIqKJYq7yAq2q4h4iCO3iD78W2qtuPBpDzsgA6dbzLz6Loa8KizTLj2EhjKqHNxx8ze0Q0GQIJOMWU4yQrVTWOg-XLyQDzk-FrKUFsPsmEYQlMCiycvUKX2vSIfrzC3teov-xeLeZbCR0VtKDVXvwoiYzXH8HQoT2TU371kUPmMVu-6xeTwYiYJID1SR-ghAPezliw%3D%3D&attredirects=1</p>
         <hr />
+        <a href="/exercicios/exercicio1.html">Exercicio 1</a>
+
     </body>
 </html>
